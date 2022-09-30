ddfd8a0e3b9daa9f6e430636764edbcc3079119d
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-09-30 15:44:07 +0800

    Added logic for addDomain in controller and service

--- app/http/controllers/Admin/Domains/AddDomainController.js
+++ app/http/controllers/Admin/Domains/AddDomainController.js
@@ -16,7 +16,22 @@ const addDomain = async (req, res) => {
     const validation = new Validation(body, rules);
     validation.validate();
 
-    // TODO: logic here...
+    const addDomainResult = await AddDomainService.addDomain(body);
+    if (addDomainResult.hasOwnProperty("error")) {
+      const responseObject = new ResponseObject(
+        ["ERR-ADDDOMAIN-01", "ERR-ADDDOMAIN-02"].includes(
+          addDomainResult.error.code
+        )
+          ? HttpCode.OK
+          : HttpCode.INTERNAL_SERVER_ERROR,
+        0,
+        undefined,
+        addDomainResult.error.code,
+        addDomainResult.error.message
+      );
+      res.status(responseObject.getHttpCode()).json(responseObject.getData());
+      return;
+    }
 
     const csrfToken = Tokenize.makeAuthCSRF(Date.now(), session.auth.user);
     req.session.auth = {
@@ -33,7 +48,7 @@ const addDomain = async (req, res) => {
       maxAge: 1000 * 60 * 60, // 1 hour validity
     });
 
-    const responseObject = new ResponseObject(HttpCode.OK, 1);
+    const responseObject = new ResponseObject(HttpCode.OK, 1, addDomainResult);
     res.status(responseObject.getHttpCode()).json(responseObject.getData());
   } catch (err) {
     if (err instanceof ValidationException) {
--- app/services/Admin/Domains/AddDomainService.js
+++ app/services/Admin/Domains/AddDomainService.js
@@ -1,11 +1,67 @@
-const moment = require("moment");
-
 const Domains = require("../../../models/Domains");
+const Workspaces = require("../../../models/Workspaces");
 
-const rules = {};
+const rules = {
+  ownerUserId: "required|integer",
+  creatorUserId: "required|integer",
+  workspaceId: "required|integer",
+  name: "required|string",
+  description: "present|string",
+};
 
-const errors = {};
+const errors = {
+  1: {
+    code: "ERR-ADDDOMAIN-01",
+    message: "Workspace, owner, and creator are unrelated to each other.",
+  },
+  2: {
+    code: "ERR-ADDDOMAIN-02",
+    message: "Domain already exists.",
+  },
+  3: {
+    code: "ERR-ADDDOMAIN-03",
+    message: "Unable to create domain record.",
+  },
+};
+
+const addDomain = async body => {
+  const { ownerUserId, creatorUserId, workspaceId, name, description } = body;
+
+  const workspaceMember = await Workspaces.relatedQuery("members")
+    .select("id")
+    .for(workspaceId)
+    .where("user_id", creatorUserId)
+    .where("owner_user_id", ownerUserId)
+    .first();
+  if (!workspaceMember) {
+    return { error: errors[1] };
+  }
+
+  const existingDomain = await Domains.query()
+    .select("id")
+    .where("owner_user_id", ownerUserId)
+    .where("workspace_id", workspaceId)
+    .where("name", name)
+    .first();
+  if (existingDomain) {
+    return { error: errors[2] };
+  }
+
+  const newDomain = await Domains.query().insertAndFetch({
+    owner_user_id: ownerUserId,
+    creator_user_id: creatorUserId,
+    workspace_id: workspaceId,
+    name,
+    description: description ? description : "",
+  });
+  if (!newDomain) {
+    return { error: errors[3] };
+  }
+
+  return { domain: newDomain };
+};
 
 module.exports = {
   rules,
+  addDomain,
 };
